dd774e2055fc958ed935ec83a54ca4c633ddbc37
Rachel Ralston <rachelralston@gmail.com>
2017-09-15 14:36:06 -0700

    WIP on contract

--- insecure-final.sol
+++ insecure-final.sol
@@ -1,39 +1,80 @@
 pragma solidity ^0.4.9;
 
 // THIS CONTRACT CONTAINS BUGS - DO NOT USE
-contract InsecureAndMessy {
-   mapping(address => uint) shares;
-   address owner;
-   address[] allShareholders;
-   /*event FailedSend(address, uint);*/
+contract SecureAndOrganized {
+  address owner;
+  struct shareData {
+    uint shareCount;
+    bool isShareholder;
+  }
+  mapping(address => shareData) shares;
+  address[] private shareholderList;
+  bool isLocked;
 
-   function InsecureAndMessy() {
-      owner = msg.sender;
-   }
 
-   function pay() payable {
-      shares[msg.sender] += msg.value;
-   }
 
-   function addShareholder(address newShareholder) constant {
-      require(msg.sender == owner);
-      allShareholders.push(newShareholder);
-   }
+  /*event FailedSend(address, uint);
+  event*/
 
-   function withdraw() {
-     var ownerShare = shares[msg.sender];
-     shares[msg.sender] = 0;
-     msg.sender.transfer(ownerShare);                             //transfer raises an exception on failure, so can take out the else.
-   }
+  modifier onlyBy(address _account){
+    require(msg.sender == _account);
+    _;
+  }
 
-   function dispense() {
-      require(msg.sender == owner);
-      address shareholder;
-      for (uint i = 0; i < allShareholders.length; i++) {
-         shareholder = allShareholders[i];
-         uint sh = shares[shareholder];
-         shares[shareholder] = 0;
-         shareholder.send(sh);
-      }
-   }
+  modifier isShareholder() {
+    require(shares[msg.sender].isShareholder);
+    _;
+  }
+
+  modifier noReentrance() {
+    require(!isLocked);
+    isLocked = true;
+    _;
+    isLocked = false;
+  }
+
+  ///////////////////////////////////////////////
+
+  function SecureAndOrganized() {
+    owner = msg.sender;
+  }
+
+  function addShares()
+  payable
+  onlyBy(owner)
+  isShareholder
+  {
+    if(msg.value > 0) shares[msg.sender] += msg.value;
+  }
+
+  function addShareholder(address newShareholder)
+  onlyBy(owner)
+  constant
+  {
+    shareholderList.push(newShareholder);
+  }
+
+  function withdraw()
+  onlyBy(owner)
+  {
+    var ownerShare = shares[msg.sender];
+    shares[msg.sender] = 0;
+    msg.sender.transfer(ownerShare);                             //transfer raises an exception on failure, so can take out the else.
+  }
+
+  function dispense()
+  onlyBy(owner)
+  noReentrance
+  returns (bool success)
+  {
+    address _shareholder;
+    for (uint i = 0; i < shareholderList.length; i++) {
+      /*if(shares[shareholderList[i]] > 0)*/
+      _shareholder = shareholderList[i];
+      uint sh = shares[_shareholder];
+      shares[_shareholder] = 0;
+      _shareholder.send(sh);
+    }
+    returns success
+  }
 }
